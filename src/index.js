01b863306b1b4e199f4c635722bf77884a540151
Irina R <49039583+iirinari@users.noreply.github.com>
2022-08-03 10:37:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,8 +34,6 @@ let months = [
 let month = months[now.getMonth()];
 h5.innerHTML = `${day}, ${hours}:${minutes}, ${month} ${currentDate}/${year}`;
 
-
-
 function displayWeatherCondition(response) {
 	celsiusTemperature = response.data.main.temp;
 
@@ -103,15 +101,79 @@ celsiuslink.addEventListener("click", convertToCelsius);
 let celsiusTemperature = null;
 
 
-function showPosition(position) {
-	let apiKey = "30d625d03dd23e91996028ac924ded11";
-	let latitude = position.coords.latitude;
-	let longitude = position.coords.longitude;
+search("Lviv");
 
-	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-	axios.get(apiUrl).then(displayWeatherCondition);
+function formatDay(timestamp) {
+	let date = new Date(timestamp * 1000);
+	let day = date.getDay();
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+	return days[day];
 }
 
+function displayForecast(response) {
+	let forecast = response.data.daily;
 
+	let forecastElement = document.querySelector("#forecast");
 
-search("Lviv");
+	let forecastHTML = `<div class="row">`;
+	forecast.forEach(function (forecastDay, index) {
+		if (index < 6) {
+			forecastHTML =
+				forecastHTML +
+				`
+      <div class="col-2">
+                <div class="card">
+                  <div class="card-body">
+                    <ul>
+                      <li class="day">${formatDay(forecastDay.dt)}</li>
+                      <li class="">
+                      <img class="animated-icon" src="http://openweathermap.org/img/wn/${
+																							forecastDay.weather[0].icon
+																						}@2x.png" alt="" width="55" /></li>
+                      <li class="day-temperature">
+                      <strong> ${Math.round(forecastDay.temp.max)}°</strong>⁄
+                      ${Math.round(forecastDay.temp.min)}°</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+  `;
+		}
+	});
+
+	forecastHTML = forecastHTML + `</div>`;
+	forecastElement.innerHTML = forecastHTML;
+	console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+	let apiKey = "30d625d03dd23e91996028ac924ded11";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(displayForecast);
+}
+
+function displayWeatherCondition(response) {
+	celsiusTemperature = response.data.main.temp;
+
+	document.querySelector("#temperatureNow").innerHTML =
+		Math.round(celsiusTemperature);
+	document.querySelector("#city").innerHTML = response.data.name;
+	document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+	document.querySelector("#wind").innerHTML = Math.round(
+		response.data.wind.speed
+	);
+	document.querySelector("#description").innerHTML =
+		response.data.weather[0].main;
+	document.querySelector("#feels-like").innerHTML = Math.round(
+		response.data.main.feels_like
+	);
+
+	let iconElement = document.querySelector("#current-weather-icon");
+	iconElement.setAttribute(
+		"src",
+		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+	);
+
+	getForecast(response.data.coord);
+}
